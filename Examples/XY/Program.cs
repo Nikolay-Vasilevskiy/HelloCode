b52717818dc7683d838bd102420d63d8e5ee9332
Oksana <caps14@gmail.com>
2022-04-17 15:41:20 +0800

    XY

---
+++ Examples/XY/Program.cs
@@ -0,0 +1,24 @@
+﻿int x, y;
+x = new Random (). Next (-100, 100);
+y = new Random (). Next (-100, 100);
+System.Console.WriteLine($"x={x}, y ={y}");
+if(x == 0| y == 0)
+{
+System.Console.WriteLine("Не принадлежит конкретной четверти");
+}
+else if(x > 0 & y > 0)
+{
+System.Console.WriteLine("Точка принадлежит 1-й плоскости");
+}
+else if(x > 0 & y < 0)
+{
+ System.Console.WriteLine("Точка принадлежит 2-й плоскости");   
+}
+else if(x < 0 & y < 0)
+{
+    System.Console.WriteLine("Точка принадлежит 3-й плоскости");
+    }
+    else if(x < 0 & y > 0)
+    {
+      System.Console.WriteLine("Точка принадлежит 4-й плоскости");  
+    }
