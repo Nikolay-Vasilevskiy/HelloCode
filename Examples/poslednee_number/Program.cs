b3be0ccd51094a0760293f4a12bb214648819b4c
Oksana <caps14@gmail.com>
2022-04-19 00:17:42 +0800

    poslednee_number

---
+++ Examples/poslednee_number/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти последнее число из трехзначного числа
+// Первый способ
+//int num = 456;
+//Console.WriteLine(num%10);
+
+//Второй способ
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(number%10);
