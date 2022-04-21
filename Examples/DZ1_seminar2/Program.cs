966554754aa95dac65aedccf751099d830fa6a3c
Oksana <caps14@gmail.com>
2022-04-21 17:23:36 +0800

    DZ1_seminar2

---
+++ Examples/DZ1_seminar2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int a = new Random().Next(100,1000);
+int b = a / 100;
+int c = a % 10;
+
+
+System.Console.WriteLine(a);
+System.Console.WriteLine(b);
+System.Console.WriteLine(c);
+System.Console.WriteLine(c & b);
+
