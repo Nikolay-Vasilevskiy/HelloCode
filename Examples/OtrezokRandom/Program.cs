9b713d7511c6c9aa516f5d835dfe8feb16870e81
Oksana <caps14@gmail.com>
2022-04-22 17:29:23 +0800

    OtrezokRandom

---
+++ Examples/OtrezokRandom/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести случайное число на отрезке (10,99) и показать наибольшую цифру числа
+int a = new Random().Next(10,100);
+System.Console.WriteLine($"случайное двухзначное число {a}");
+int b = a / 10;
+int c = a % 10;
+if (b > c)
+{
+ System.Console.WriteLine ($" бОльшее число {b}");   
+}
+else
+{
+   System.Console.WriteLine ($" бОльшее число {c}"); 
+}
+
