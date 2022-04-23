17052664c0aa9ba39795f5169bc76379282e19e5
Oksana <caps14@gmail.com>
2022-04-23 22:05:50 +0800

    ДЗ№27

---
+++ Examples/ДЗ№27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+ Console.WriteLine("Введите число");
+int size = int.Parse(Console.ReadLine());
+    
+int i;
+
+    for (i = size == 0 ? 0 : 0; size > 0; size /= 10)
+    {
+        i += size %10; //i = i + size %10;
+    }
+
+System.Console.WriteLine(i);
+
+
