daaa9861b9a4beaf24cd58a5b96d1b5f21cf5a0c
Oksana <caps14@gmail.com>
2022-04-23 17:52:25 +0800

    N

---
+++ Examples/N/Program.cs
@@ -0,0 +1,10 @@
+﻿// Произведение чисел от одного до N( N=5-> 1*1,2*3,6*4,24*5)
+Console.Clear();
+System.Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+int x = 1;
+for (int i = 1; i <= A; i ++)
+{
+    x *= i;
+    System.Console.Write(x + " ");
+}
