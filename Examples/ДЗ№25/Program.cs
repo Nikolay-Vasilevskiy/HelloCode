11b8dbc24bf705c494ffbdc756752f204f378a3e
Oksana <caps14@gmail.com>
2022-04-23 18:44:55 +0800

    ДЗ№25

---
+++ Examples/ДЗ№25/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+System.Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A^B = " + Math.Pow(A, B)); 
