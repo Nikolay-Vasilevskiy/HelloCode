c11ade36ca03bd5712141820fc4ff964c7bb6909
Oksana <caps14@gmail.com>
2022-04-23 22:32:50 +0800

    ДЗ№29

---
+++ Examples/ДЗ№29/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int [] mas = new int[8];
+for (int i = 0; i < mas.Length; i++)
+{
+mas[i] = new Random().Next(0,10);
+System.Console.Write(mas[i]);
+}
