e7b2b95b0adffb3c48635b84c78e05cf79a11fce
Oksana <caps14@gmail.com>
2022-04-25 01:10:22 +0800

    Seminar1_0

---
+++ Examples/Seminar1_0/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход
+//принимает число и выдаёт его квадрат (число
+//умноженное на само себя)
+
+//int number;
+//number = 5;
+//number = number * number;
+//Console.Write("Квадрат равен ");
+//Console.WriteLine(number);
+
+//Красивый способ
+
+int number, numberSquare;
+number = -11;
+numberSquare = number * number;
+Console.Write("Квадрат  ");
+Console.WriteLine(number);
+Console.Write(" равен  ");
+Console.WriteLine(numberSquare);
+
+//способ записи в одну строку
+
+Console.WriteLine($"Квадрат {number} равен {numberSquare}");
