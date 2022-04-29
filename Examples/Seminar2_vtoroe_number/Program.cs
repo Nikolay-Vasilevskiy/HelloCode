9d91c543ffe1786da90ab947b4d3b00a106b7999
Oksana <caps14@gmail.com>
2022-04-29 18:38:38 +0800

    Seminar2_vtoroe_number

--- Examples/Seminar2_vtoroe_number/Program.cs
+++ Examples/Seminar2_vtoroe_number/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Найти второе число из трехзачного числа
-int number = 456;
+//int number = 456;
+System.Console.WriteLine("Введите трёхзначное число number: ");
+int number = int.Parse(Console.ReadLine());
 number = number %100;
 number = number /10;
 System.Console.WriteLine(number);
