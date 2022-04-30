70078567a62dadf4c45c2853d94f0cca1c753f07
Oksana <caps14@gmail.com>
2022-05-01 00:03:07 +0800

    DZ4_seminar2_Days

---
+++ Examples/DZ4_seminar2_Days/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,является ли этот день выходным.
+int day = new Random().Next(1,8);
+System.Console.WriteLine($"День недели {day}");
+if (day == 1)
+{
+    System.Console.WriteLine ($"Понедельник"); 
+}
+if (day == 2)
+{
+    System.Console.WriteLine ($"Вторник"); 
+}
+if (day == 3)
+{
+    System.Console.WriteLine ($"Среда"); 
+}
+if (day == 4)
+{
+    System.Console.WriteLine ($"Четверг"); 
+}
+if (day == 5)
+{
+    System.Console.WriteLine ($"Пятница"); 
+}
+if (day == 6)
+{
+    System.Console.WriteLine ($"Суббота"); 
+}
+if (day == 7)
+{
+    System.Console.WriteLine ($"Воскресение"); 
+}
+if (day == 6)
+{
+    System.Console.WriteLine ($"Выходной день"); 
+}
+if (day == 7)
+{
+    System.Console.WriteLine ($"Выходной день"); 
+}
