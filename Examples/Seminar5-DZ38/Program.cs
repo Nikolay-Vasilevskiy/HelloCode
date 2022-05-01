fe727d6436f2722d9b84352b7133f3906ca29dcf
Oksana <caps14@gmail.com>
2022-05-01 13:30:18 +0800

    Seminar5-DZ38

---
+++ Examples/Seminar5-DZ38/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите
+//разницу между максимальным и минимальным
+//элементов массива.
+//[3 7 22 2 78] -> 76
+
+double R; // разность между максимальным и минимальным элементом
+double [] mass = new double [4];
+for ( int i = 0; i < mass.Length; i++)
+{
+ mass[i] = new Random().Next(0,100);
+ Console.WriteLine(mass[i] + " ");
+}
+ double max = double.MinValue;
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i] > max)
+    {
+        // найден больший элемент
+        max = mass[i];
+    }
+    
+}
+double min = double.MaxValue;
+for (int i = 0; i < mass.Length; i++)
+{
+if (mass[i] < min)
+    {
+        // найден наименьший элемент
+        min = mass[i];
+    }
+}
+R = max - min;
+Console.WriteLine($"Максимальный элемент массива:{max}");
+Console.WriteLine($"Минимальный элемент массива:{min}");
+Console.WriteLine($"Разность:{R}");
